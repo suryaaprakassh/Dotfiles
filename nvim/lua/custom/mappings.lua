83753511c7f63ba4c649fe17a4f396b49aff29da
suryaprakash <suryaaprakassh@gmail.com>
2023-11-23 09:11:02 +0530

    added keymaps

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -8,11 +8,21 @@ M.general = {
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
     ["<leader>p"] = { '"+p', "Paste System Clipboard" },
+    ["<leader>y"] = { ' "+y ', "Copy System Clipboard" },
+    ["<leader>d"] = { ' "_d ', "Delete" },
   },
-
   v = {
     ["<leader>y"] = { ' "+y ', "Copy System Clipboard" },
+    ["<leader>d"] = { ' "_d ', "Delete" },
+    ["J"]={":m '>+1<CR>gv=gv","Move lines Down"},
+    ["K"]={":m '<-2<CR>gv=gv","Move lines Up"}
   },
+
+  x={
+    ["<leader>p"]={'[[" _dP]]'}
+  }
 }
 
 return M
+
+
