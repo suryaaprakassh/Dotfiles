6b472b0a4261a995f6c5133118bdc93eced55376
suryaprakash <surya0prakash@proton.me>
2024-08-23 22:03:23 +0530

    rice

---
+++ conf/.config/Thunar/accels.scm
@@ -0,0 +1,5 @@
+; thunar GtkAccelMap rc-file         -*- scheme -*-
+; this file is an automated accelerator map dump
+;
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1622790717174452-1" "F3")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1581593230771688-1" "F4")
